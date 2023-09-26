435a32b4fb93decbc0a5f8158573c1de5a82fa6a
semnil5202 <semnil5202@gmail.com>
2023-09-26 16:05:12 +0900

    storybook: 스토리북 배포링크 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import './App.css';
+import styled from 'styled-components';
+import Tabs from './lib/Tabs';
+import Tab from './lib/Tab';
 
 function App() {
-  return <>app</>;
+  return (
+    <Tabs width={1000} responsive swiper $simpleTab>
+      <Tab label="number1">
+        <Wrapper>sadfsaf</Wrapper>
+      </Tab>
+      <Tab label="number2">content2</Tab>
+      <Tab label="number3">content3</Tab>
+      <Tab label="number3">content3</Tab>
+      <Tab label="number3">content3</Tab>
+      <Tab label="number3">content3</Tab>
+      <Tab label="number3">content3</Tab>
+      <Tab label="number3">content3</Tab>
+      <Tab label="number3">content3</Tab>
+    </Tabs>
+  );
 }
+
+const Wrapper = styled.div`
+  width: 1000px;
+  height: 1000px;
+  background-color: red;
+  color: white;
+`;
 
 export default App;
